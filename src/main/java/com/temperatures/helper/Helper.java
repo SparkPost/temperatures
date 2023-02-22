337127be073166f5877d085df2cc004ff512e5be
param.raghavan@messagebird.com <param.raghavan@messagebird.com>
2023-02-22 15:51:25 -0500

    .

--- src/main/java/com/temperatures/helper/Helper.java
+++ src/main/java/com/temperatures/helper/Helper.java
@@ -2,6 +2,7 @@ package com.temperatures.helper;
 
 import com.temperatures.cargo.LineOfText;
 import com.temperatures.cargo.ParsedRecord;
+import com.temperatures.key.ParsedRecordsKey;
 
 public class Helper {
     // "Region,Country,State,City,Month,Day,Year,AvgTemperature";
@@ -11,5 +12,10 @@ public class Helper {
                 new ParsedRecord(tokens[0],tokens[1], tokens[2], tokens[3],
                         Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), Integer.parseInt(tokens[6]),
                         Double.parseDouble(tokens[7]));
+    }
+
+    public static ParsedRecordsKey parseRecordKey(ParsedRecord pr) {
+        return
+                new ParsedRecordsKey(pr.getRegion(), pr.getCountry(), pr.getState(), pr.getCity(), pr.getMonth(), pr.getYear());
     }
 }
